1fd730ada161e94f8a1418cddf21a602a6f49fe5
Karan <karan.kumar.c@gmail.com>
2016-07-21 12:37:15 +0530

    Strict mode setup

--- app/src/main/java/com/pluralsight/karan/appthreadissues/MainActivity.java
+++ app/src/main/java/com/pluralsight/karan/appthreadissues/MainActivity.java
@@ -1,9 +1,18 @@
 package com.pluralsight.karan.appthreadissues;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MAX_WRITES = 5;
 
     /**
      * Starting point of the activity
@@ -13,5 +22,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void btnWriteToFileOnClick(Button view){
+
+    }
+
+    private FileOutputStream openOutStream(String filename) {
+        FileOutputStream outputStream = null;
+
+        try{
+            outputStream = openFileOutput(filename, MODE_PRIVATE);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return outputStream;
+    }
+
+    protected void closeOutStream(FileOutputStream outStream){
+        try{
+            if(outStream != null){
+                outStream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void simpleWrite(FileOutputStream outStream, String buffer){
+        try{
+            outStream.write(buffer.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
