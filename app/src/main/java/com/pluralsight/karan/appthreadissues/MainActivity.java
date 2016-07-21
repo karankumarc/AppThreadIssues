f8c79cb6bde68e787b95ce0130f48f948be090e5
Karan <karan.kumar.c@gmail.com>
2016-07-21 13:52:44 +0530

    Async write setup

--- app/src/main/java/com/pluralsight/karan/appthreadissues/MainActivity.java
+++ app/src/main/java/com/pluralsight/karan/appthreadissues/MainActivity.java
@@ -1,10 +1,14 @@
 package com.pluralsight.karan.appthreadissues;
 
+import android.content.DialogInterface;
 import android.os.StrictMode;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -32,6 +36,52 @@ public class MainActivity extends AppCompatActivity {
             simpleWrite( outStream, "Hello World");
         }
         closeOutStream(outStream);
+    }
+
+    public void btnWriteToFileAsyncTask(View view){
+        String messageToWrite = "Message to write to file";
+
+        ProgressBar pb = (ProgressBar) findViewById(R.id.progressBarMain);
+        initializeProgressBar(pb);
+        displayStartedMessage();
+
+        FileOutputStream outStream = openOutStream("testout.dat");
+        for (int i = 0; i < MAX_WRITES; i++) {
+            slowWrite(outStream, messageToWrite);
+            pb.setProgress(i);
+        }
+
+        closeOutStream(outStream);
+
+        displayCompletionMessage();
+        cleanupProgressBar(pb);
+    }
+
+    protected void initializeProgressBar(ProgressBar pb) {
+        pb.setMax(MAX_WRITES);
+        pb.setProgress(0);
+        pb.setVisibility(View.VISIBLE);
+    }
+
+    protected void displayStartedMessage() {
+        Toast.makeText(this, "File operation started", Toast.LENGTH_SHORT).show();
+    }
+
+    protected void cleanupProgressBar(ProgressBar pb) {
+        pb.setVisibility(View.INVISIBLE);
+    }
+
+    protected void displayCompletionMessage() {
+        AlertDialog.Builder bldr = new AlertDialog.Builder(MainActivity.this);
+        bldr.setTitle("Write to File")
+                .setMessage("File operation complete")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                }).show();
     }
 
     private FileOutputStream openOutStream(String filename) {
@@ -64,6 +114,15 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+    protected void slowWrite(FileOutputStream outStream, String buffer){
+        try{
+            outStream.write(buffer.getBytes());
+            Thread.sleep(1500);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
